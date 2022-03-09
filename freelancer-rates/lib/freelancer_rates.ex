df87ac263372b09a8a621f1fe6d54c6287b501d7
Cyytrus <antoniopaulo122@gmail.com>
2022-03-09 09:11:12 -0300

    Add: Lasagna, Freelancer-rates and pacman-rules exercises

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,17 +1,20 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
-    # Please implement the daily_rate/1 function
+    hourly_rate * 8.0
   end
 
   def apply_discount(before_discount, discount) do
-    # Please implement the apply_discount/2 function
+    (before_discount * (1 - discount / 100.0))
+    |> Float.floor(5)
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
+    apply_discount(hourly_rate * 8.0 * 22, discount)
+    |> ceil()
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    # Please implement the days_in_budget/3 function
+    (budget / apply_discount(hourly_rate * 8.0, discount))
+    |> Float.floor(1)
   end
 end
--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,11 +1,12 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
 
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def expected_minutes_in_oven(), do:
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def remaining_minutes_in_oven()
 
-  # Please define the 'total_time_in_minutes/2' function
+  def preparation_time_in_minutes()
 
-  # Please define the 'alarm/0' function
+  end
+
+  def alarm()
 end
